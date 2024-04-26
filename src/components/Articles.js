6b8f47a945fc4f5bc9ae95ce155f516b00723f88
Darien Moment <dmoment18@gmail.com>
2024-04-26 03:25:05 -0400

    Finished desktop version

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import left from '../assets/left-white-arrow.png';
+import right from '../assets/right-white-arrow.png';
+import up from '../assets/up-white-arrow.png';
+import down from '../assets/down-white-arrow.png';
+import { articledata } from '../data/articles';
 
 const ArticleWrapper = styled.div`
     padding-top:5vh;
@@ -15,17 +20,186 @@ const Title = styled.div`
     text-align: center;
     word-wrap:break-word;
     font-family: 'Lora,serif';
-
+    padding-bottom: 5rem;
     @media (max-width: 768px) {
         font-size: 2em;
     }
 `;
 
+const Map = styled.div`
+`;
+
+const UpArrow = styled.div`
+    width: 10%;
+    height: 5%;
+    margin: auto;
+    cursor: ${({ clickable }) => (clickable ? 'pointer' : 'not-allowed')};
+    opacity: ${({ clickable }) => (clickable ? 1 : 0.5)};
+    & img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const Center = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const LeftArrow = styled.div`
+    width: 10%;
+    height: 5%;
+    cursor: ${({ clickable }) => (clickable ? 'pointer' : 'not-allowed')};
+    opacity: ${({ clickable }) => (clickable ? 1 : 0.5)};
+    & img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const ArticlesBox = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    margin-top: 1rem;
+    margin-bottom: 4rem;
+
+    transition: transform 0.3s ease;
+`;
+
+const RightArrow = styled.div`
+    width: 10%;
+    height: 5%;
+    cursor: ${({ clickable }) => (clickable ? 'pointer' : 'not-allowed')};
+    opacity: ${({ clickable }) => (clickable ? 1 : 0.5)};
+    & img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const DownArrow = styled.div`
+    width: 10%;
+    height: 5%;
+    margin: auto;
+    cursor: ${({ clickable }) => (clickable ? 'pointer' : 'not-allowed')};
+    opacity: ${({ clickable }) => (clickable ? 1 : 0.5)};
+    & img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const ArticleInfo = styled.div`
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    font-family: 'Lora,serif';
+`;
+
+const Year = styled.div`
+    margin-top: 2rem;
+    font-weight: bold;
+    font-size: 3rem;
+`;
+
+const Article = styled.div`
+    background:
+        /* top, transparent black, faked with gradient */ 
+        linear-gradient(
+          rgba(0, 0, 0, 0.5), 
+          rgba(0, 0, 0, 0.5)
+        ),
+        /* bottom, image */
+        url(${props => props.image});
+
+    background-position: center;
+    background-size: cover;
+    text-align: center;
+    width: 30vw;
+    height: 24vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const ArticleTitle = styled.div`
+    font-size: 3rem;
+    text-shadow: 5px 5px 2px black;
+`
+
+const ArticleDate = styled.div`
+    font-size: 1.5rem;
+`
+
 const Articles = () => {
+    const [currentYearIndex, setCurrentYearIndex] = useState(0);
+    const [currentArticleIndex, setCurrentArticleIndex] = useState(0);
+    const years = Object.keys(articledata);
+    const articles = articledata[years[currentYearIndex]];
+
+    const handleLeftArrow = () => {
+        if (currentArticleIndex > 0) {
+            setCurrentArticleIndex(prevIndex => prevIndex - 1);
+        }
+    };
+
+    const handleRightArrow = () => {
+        if (currentArticleIndex < articles.length - 1) {
+            setCurrentArticleIndex(prevIndex => prevIndex + 1);
+        }
+    };
+
+    const handleUpArrow = () => {
+        if (currentYearIndex > 0) {
+            setCurrentYearIndex(prevIndex => prevIndex - 1);
+            setCurrentArticleIndex(0); // Reset article index when changing year
+        }
+    };
+
+    const handleDownArrow = () => {
+        if (currentYearIndex < years.length - 1) {
+            setCurrentYearIndex(prevIndex => prevIndex + 1);
+            setCurrentArticleIndex(0); // Reset article index when changing year
+        }
+    };
+
+    const leftArrowClickable = currentArticleIndex > 0;
+    const rightArrowClickable = currentArticleIndex < articles.length - 1;
+    const upArrowClickable = currentYearIndex > 0;
+    const downArrowClickable = currentYearIndex < years.length - 1;
+
+
     return (
         <ArticleWrapper>
             <Title>EXPLORE PIECES</Title>
-            
+            <Map>
+                <UpArrow clickable={upArrowClickable} onClick={handleUpArrow}><img src={up}/></UpArrow>
+                <Center>
+                    <LeftArrow clickable={leftArrowClickable} onClick={handleLeftArrow}><img src={left}/></LeftArrow>
+                    <ArticleInfo>
+                        <Year>{years[currentYearIndex]}</Year>
+                        <ArticlesBox>
+                            <a href={articles[currentArticleIndex].article_link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
+                                <Article image={articles[currentArticleIndex].image_url}>
+                                    <ArticleTitle>{articles[currentArticleIndex].article_title}</ArticleTitle>
+                                    <ArticleDate>{articles[currentArticleIndex].article_date}</ArticleDate>
+                                </Article>
+                            </a>
+                            {articles[currentArticleIndex + 1] && (
+                                <a href={articles[currentArticleIndex + 1].article_link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    <Article image={articles[currentArticleIndex + 1].image_url}>
+                                        <ArticleTitle>{articles[currentArticleIndex + 1].article_title}</ArticleTitle>
+                                        <ArticleDate>{articles[currentArticleIndex + 1].article_date}</ArticleDate>
+                                    </Article>
+                                </a>
+                            )}
+                        </ArticlesBox>
+                    </ArticleInfo>
+                    <RightArrow clickable={rightArrowClickable} onClick={handleRightArrow}><img src={right}/></RightArrow>
+                </Center>
+                <DownArrow clickable={downArrowClickable} onClick={handleDownArrow}><img src={down}/></DownArrow>
+            </Map>
         </ArticleWrapper>
     );
   };
---
+++ src/data/articles.js
@@ -0,0 +1,144 @@
+export const articledata = {
+    "1960s Protests" :
+        [
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Misrepresentations",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19680507-01.2.7&e=-------en-20--1--txt-txIN-1968------",
+                article_date: "May 7, 1968"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "LTE: Holds Kirk Responsible",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19680503-01.2.13&e=-------en-20--1--txt-txIN-1968------",
+                article_date: "May 3, 1968"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "City Newspapers Distort Protests",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19680427-01.2.3&srpos=5&e=01-04-1968-31-04-1968--en-20--1--txt-txIN-protests------",
+                article_date: "April 27, 1968"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Cops Out",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19680502-01.2.11&e=-------en-20--1--txt-txIN-1968------",
+                article_date: "May 2, 1968"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "In Power's Name",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19680502-01.2.13&e=-------en-20--1--txt-txIN-1968------",
+                article_date: "May 2, 1968"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "A Glimmer of Hope",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19680427-01.2.8&srpos=10&e=01-04-1968-31-04-1968--en-20--1--txt-txIN-protests------",
+                article_date: "April 27, 1968"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "The Reconstruction",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19680501-01.2.7&e=-------en-20--1--txt-txIN-1968------",
+                article_date: "May 1, 1968"
+            }
+
+        ],
+    "1980s Protests" :
+        [
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Letters of Outrage: Discuss Divestment",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19850329-01.2.8&srpos=4&e=01-03-1985-31-03-1985--en-20--1--txt-txIN-protest------",
+                article_date: "March 29, 1985"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Day 5 of the blockade: a profile",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19850409-01.2.15&srpos=2&e=01-04-1985-31-04-1985--en-20--1--txt-txIN-protests------",
+                article_date: "April 9, 1985"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Organization marks protest",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19850424-01.2.22&srpos=13&e=01-04-1985-31-04-1985--en-20--1--txt-txIN-protests------",
+                article_date: "April 24, 1985"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Hypocrisy of protesters draws ironic parallel",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19850408-01.2.10&srpos=8&e=01-04-1985-31-04-1985--en-20--1--txt-txIN-protests------",
+                article_date: "April 8, 1985"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Divest is life or death issue",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19850404-01.2.3&srpos=16&e=01-04-1985-31-04-1985--en-20--1--txt-txIN-protest------",
+                article_date: "April 4, 1985"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Emotional Rescue",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19850325-01.2.9&srpos=1&e=01-03-1985-31-03-1985--en-20--1--txt-txIN-protest------",
+                article_date: "March 25, 1985"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "A '68 Leader on the Protests of Today",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs19870318-01.2.10&srpos=12&e=-------en-20--1--txt-txIN-1968------",
+                article_date: "March 18, 1987"
+            }
+        ],
+    "2000-2022 protests":
+        [
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Why protest?",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs20020214-01.2.16&srpos=1&e=------200-en-20--1--txt-txIN-protest------",
+                article_date: "February 14, 2002"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Students protest tenure denial",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs20121128-01.2.5&srpos=248&e=------201-en-20--241-byDA-txt-txIN-protest------",
+                article_date: "November 28, 2012"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Under surveillance",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs20120202-02.2.3&srpos=201&e=------201-en-20--201-byDA-txt-txIN-protest------",
+                article_date: "February 2, 2012"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Protest more, protest harder",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs20120131-01.2.18&srpos=200&e=------201-en-20--181-byDA-txt-txIN-protest------",
+                article_date: "January 31, 2012"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "LionPAC members protest Hamas",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs20060222-01.2.8&srpos=4&e=------200-en-20--1--txt-txIN-protest------",
+                article_date: "February 26, 2006"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "Reviving the spirit of '68",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs20111014-01.2.19&srpos=150&e=------201-en-20--141-byDA-txt-txIN-protest------",
+                article_date: "October 14, 2011"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "A time to stand",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs20110928-01.2.18&srpos=126&e=------201-en-20--121-byDA-txt-txIN-protest------",
+                article_date: "September 28, 2011"
+            },
+            {
+                image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WJ4KH7SVA5A5BK2WQQ27TC2ZQI.jpg",
+                article_title: "SJP Protest Walk",
+                article_link: "https://spectatorarchive.library.columbia.edu/?a=d&d=cs20110303-01.2.3&srpos=99&e=------201-en-20--81-byDA-txt-txIN-protest------",
+                article_date: "March 3, 2011"
+            }
+        ]
+}
